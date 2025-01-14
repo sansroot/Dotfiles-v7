b8437f5a5304848b6254c5b8eca9165192e25ac0
sansroot <sansroot@github.com>
2025-01-14 04:08:41 -0300

    new theme added, rofi theme-switcher changed

--- PLACE-IN-DOCUMENTS-DIRECTORY/themes/theme-switcher.sh
+++ PLACE-IN-DOCUMENTS-DIRECTORY/themes/theme-switcher.sh
@@ -1,1 +1,1 @@
-selected=$(ls ~/Documents/themes/scripts/ | sed 's/\.sh$//' | rofi -dmenu -p "Run: ") && bash ~/Documents/themes/scripts/$selected.sh
+selected=$(ls ~/Documents/themes/scripts/ | sed 's/\.sh$//' | rofi -dmenu -p "Run: " -theme ~/.config/rofi/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected.sh
