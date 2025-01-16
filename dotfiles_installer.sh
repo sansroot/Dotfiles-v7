1783804617ba165d3ce3fd5fa2979b4f0aee18a8
San's <fsanches2002@outlook.com>
2025-01-16 10:39:54 -0300

    Update dotfiles_installer.sh

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo "Write your network interface name (wlan0/eth0/enp3s0...): "
+read variavel
+  sed -i "s/enp3s0/$variavel/" ./waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/blue/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/dawn/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/green/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/lavender/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/mauve/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/moon/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/peach/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/pink/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/red/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/sapphire/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/tokyonight/waybar/config.jsonc
+  sed -i "s/enp3s0/$variavel/" ./themes/themes/yellow/waybar/config.jsonc
 cp -r ./fastfetch ./hypr ./kitty ./rofi ./waybar -t ~/.config
 cp -r ./themes -t ~/Documents/
 cp -r ./.zshrc ./.p10k.zsh -t ~/
