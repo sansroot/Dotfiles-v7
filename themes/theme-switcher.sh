1c7f73ac3097ebb87af57e3bfafe071fd267e261
sansroot <sansroot@github.com>
2025-01-16 02:41:42 -0300

    script to add new themes

--- themes/theme-switcher.sh
+++ themes/theme-switcher.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
 selected=$(ls ~/Documents/themes/scripts/ | sed 's/\.sh$//' | rofi -dmenu -p "Run: " -theme ~/.config/rofi/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected.sh
+if [ -z "$selected" ]; then
+    killall rofi
+    exit 0
+fi
+selected1=$(ls ~/Documents/themes/scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "Run: " -theme ~/.config/rofi/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected/$selected1.sh
