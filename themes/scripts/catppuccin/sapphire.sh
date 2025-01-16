814a63c6b7919dc41684dad72e6c551668c03c19
sansroot <sansroot@github.com>
2025-01-16 02:48:10 -0300

    script to add new themes

--- themes/scripts/catppuccin/sapphire.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -r ~/Documents/themes/themes/sapphire/* -t ~/.config/
-swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor Catppuccin Mocha Dark 25
-killall waybar
-waybar
