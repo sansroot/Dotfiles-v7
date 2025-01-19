aa3901a731e735b4fdaa07800ce38289205fe05f
sansroot <sansroot@github.com>
2025-01-19 16:39:42 -0300

    script to add cursor lines to hyprland.confs

--- themes/scripts/rose-pine/dawn.sh
+++ themes/scripts/rose-pine/dawn.sh
@@ -2,6 +2,6 @@
 
 cp -r ~/Documents/themes/themes/dawn/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-
+hyprctl setcursor hypr-dots 24
 killall waybar
 waybar
