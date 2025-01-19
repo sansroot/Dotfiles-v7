79822b89a24eb8e2c7248f2a72898c5305d65e11
sansroot <sansroot@github.com>
2025-01-19 07:20:21 -0300

    cursor change

--- themes/scripts/catppuccin/pink.sh
+++ themes/scripts/catppuccin/pink.sh
@@ -2,6 +2,6 @@
 
 cp -r ~/Documents/themes/themes/pink/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor Catppuccin Mocha Dark 25
+hyprctl setcursor Crosshair Cursors 24
 killall waybar
 waybar
