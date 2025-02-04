9d8f393890b63f6c3ec45c205b03e0035341cd05
San's <fsanches2002@outlook.com>
2025-02-04 01:32:05 -0300

    Update dotfiles_installer.sh

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -211,4 +211,4 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &> /dev/null
 gsettings set org.gnome.desktop.interface gtk-theme "hypr-dots-green" &> /dev/null
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots' &> /dev/null
 papirus-folders -C cat-mocha-green &> /dev/null
-
+killall rofi
