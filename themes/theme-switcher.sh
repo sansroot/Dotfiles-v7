5d75f88f0e2dc7701d34eb654e76b90958db7a74
sansroot <sansroot@github.com>
2025-02-04 00:37:25 -0300

    general update

--- themes/theme-switcher.sh
+++ themes/theme-switcher.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-selected=$(ls ~/Documents/themes/scripts/ | sed 's/\.sh$//' | rofi -dmenu -p "Run: " -theme ~/.config/rofi/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected.sh
+selected=$(ls ~/Documents/themes/scripts/ | sed 's/\.sh$//' | rofi -dmenu -p "Hyprland, Waybar, Rofi" -theme ~/Documents/themes/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected.sh
 if [ -z "$selected" ]; then
     killall rofi
     exit 0
 fi
-selected1=$(ls ~/Documents/themes/scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "Run: " -theme ~/.config/rofi/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected/$selected1.sh
+
+
+selected1=$(ls ~/Documents/themes/icon_theme_scripts/ | sed 's/\.sh$//' | rofi -dmenu -p "GTK Theme, Icons" -theme ~/Documents/themes/theme-switcher1.rasi) && bash ~/Documents/themes/icon_theme_scripts/$selected1.sh
+if [ -z "$selected1" ]; then
+    killall rofi
+    exit 0
+fi
