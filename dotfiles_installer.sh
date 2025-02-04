838f97eba326a41cd4d9cd8f9ae3d48057472ce0
San's <fsanches2002@outlook.com>
2025-02-03 21:47:26 -0300

    Update dotfiles_installer.sh

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -207,6 +207,7 @@ rm -rf hypr-dots
 hyprctl setcursor hypr-dots 24
 sudo chmod -R 777 /usr/share/themes
 sudo chmod -R 777 /usr/share/icons
+sudo chmod -R 777 /usr/bin/papirus-folders
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &> /dev/null
 gsettings set org.gnome.desktop.interface gtk-theme "hypr-dots-mauve" &> /dev/null
 papirus-folders -C cat-mocha-mauve &> /dev/null
