8c3eb7e966ab4fc6fd891b41fa2cfa9ad87b2c1e
San's <fsanches2002@outlook.com>
2025-02-08 01:30:24 -0300

    Update dotfiles_installer.sh

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -170,6 +170,7 @@ if [ "$option" == "Y" ]; then
         sed -i "s/monitor:DP-2/monitor:DP-1/" ./themes/themes/tokyo-night/hypr/hyprland.conf &>/dev/null
         sed -i "s/monitor:DP-2/monitor:DP-1/" ./themes/themes/yellow/hypr/hyprland.conf &>/dev/null
     fi
+    clear
     echo "would you like to set the gaps manually? [Y/N]"
     read gaps
     gaps="${gaps^^}"
@@ -186,8 +187,7 @@ if [ "$option" == "Y" ]; then
             sed -i "s/gaps_out = 40/gaps_out = $gaps_out/" ./themes/themes/$item/hypr/hyprland.conf
         done
     fi
-
-
+    clear
     echo "would you like to set a custom opacity? [Y/N]"
     read opacity
     opacity="${opacity^^}"
@@ -204,13 +204,9 @@ if [ "$option" == "Y" ]; then
             sed -i "s/inactive_opacity = 0.3/inactive_opacity = $inactive_opacity/" ./themes/themes/$item1/hypr/hyprland.conf
         done
     fi
-
-
-
 else
     echo "When you log on hyprland, run this script again to set you monitor and refresh rate" 
     sleep 4   
-
 fi
 clear
 ifconfig
